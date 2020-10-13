b04deaf33b34161122ba5ca12a7c16f2e06b9a8d
gustavR0 <gust040825@gmail.com>
2020-10-13 08:16:51 -0500

    error angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-inegi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-inegi');
+    expect(app.title).toEqual('angular');
   });
 
   it('should render title', () => {
