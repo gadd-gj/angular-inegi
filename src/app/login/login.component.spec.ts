51b1514fe4701ffbe0e3b332e54f0f2d7a915c94
gadd-gj <gaddielgomez15@gmail.com>
2020-12-12 19:15:40 -0600

    Correccion de errores

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { RouterTestingModule } from "@angular/router/testing";
 import { LoginComponent } from './login.component';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +10,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -22,4 +25,49 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+
+  it('Llamar a la funcion valUser()', () => {
+    let res = null;
+
+    component.username = 'gustavo@gmail.com';
+    component.password = '123456';
+
+    component.valUser();
+    res = component.res;
+
+    expect(res).toBe(true);
+
+  });
+
+
+  it('Llamar a la funcion validar() para validar que los campos no esten vacios', () => {
+
+    let res = '';
+
+    component.username = '';
+    component.password = '';
+
+    component.validar();
+    res = component.respuesta;
+
+    expect(res).toBe('Los campos están vacíos');
+
+  });
+
+  it('Llamar a la funcion validar() para validar usuario y contraseña', () => {
+
+    let res = '';
+
+    component.username = 'gadd@gmail.com';
+    component.password = '123789';
+
+    component.validar();
+    res = component.respuesta;
+
+    expect(res).toBe('Usuario y/o contraseña incorrecto');
+
+  });
+
 });
--- src/app/signin/signin.component.spec.ts
+++ src/app/signin/signin.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from "@angular/router/testing";
 import { SigninComponent } from './signin.component';
 
 describe('SigninComponent', () => {
@@ -8,6 +8,7 @@ describe('SigninComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ SigninComponent ]
     })
     .compileComponents();
@@ -23,12 +24,100 @@ describe('SigninComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Los campos no pueden estar vacios', () => {
+  it('Llamar a la funcion vacio()', () => {
 
+    //Arrange
+    let res = false;
+
+    component.name = '';
+    component.email = '';
+    component.password = '';
+    //Act
     component.vacio();
-
-    expect(component.valUser()).toBeFalse();
+    res = component.res;
+    //Assert
+    expect(res).toBe(false);
+    
   
+  });
+
+  it('Llamar a la funcion valUser()', () => {
+    let res = true;
+
+    component.email = 'gaddiel@gmail.com';
+
+    component.valUser();
+    res = component.res;
+
+    expect(res).toBe(false);
+
+  });
+
+  it('Llamar a la funcion validar() para validar que los campos no esten vacios', () =>{
+    
+    let res = '';
+
+    component.age = null;
+    component.name = '';
+    component.email = '';
+    component.password = '';
+
+    component.validar();
+    res = component.respuesta;
+
+    expect(res).toBe('Los campos están vacíos');
+
+  });
+
+
+  it('Llamar a la funcion validar() para verificar el usuario', () =>{
+    
+    let res = '';
+
+    component.age = 20;
+    component.name = 'jorge';
+    component.email = 'gustavo@gmail.com';
+    component.password = '123456';
+
+    component.validar();
+    res = component.respuesta;
+
+    expect(res).toBe('El usuario existe');
+
+  });
+
+  it('Llamar a la funcion validar() para verificar la edad', () =>{
+    
+    let res = '';
+
+    component.age = 140;
+    component.name = 'gadd';
+    component.email = 'gaddiel@gmail.com';
+    component.password = '123456';
+
+
+    component.validar();
+    res = component.respuesta;
+
+    expect(res).toBe('La edad no es valida');
+
+  });
+
+
+  it('Llamar a la funcion validar() para validar el correo', () =>{
+    
+    let res = '';
+
+    component.age = 50;
+    component.name = 'jorge';
+    component.email = 'jorge.com';
+    component.password = '123456';
+
+    component.validar();
+    res = component.respuesta;
+
+    expect(res).toBe('El correo no es valido');
+
   });
 
 });
