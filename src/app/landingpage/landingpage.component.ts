ddf62200b19bf07d9056614a7ade8c5b90eaf76a
Arely CR <arelycr07@hotmail.com>
2020-12-18 02:37:14 -0600

    add angularlanding #7

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-landingpage',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  isLogin(): boolean{
+    return this.auth.isAuth();
   }
 
 }
